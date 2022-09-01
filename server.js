1593aab8e39f54cc5523691c040664eb7d23ebd3
antoniowav <85946713+antoniowav@users.noreply.github.com>
2022-09-01 11:02:34 +0200

    server structure and front end client structure

--- server.js
+++ server.js
@@ -1,19 +1,47 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
-import express from 'express'
+import express from "express";
 
-const app = express()
-const httpServer = createServer(app); 
+const app = express();
+const httpServer = createServer(app);
 const port = 3000;
-const io = new Server(httpServer)
+const io = new Server(httpServer);
 
-app.use("/", express.static("./client"))
+app.use("/", express.static("./client"));
+
+let users = []; // Users array
 
 io.on("connection", (socket) => {
-    console.log(`Socket witd ID: ${socket.id} has connected!`);
-})
+  console.log(`Socket witd ID: ${socket.id} has connected!`);
 
+  socket.on("login", ({ name, room }, callback) => {
+    //socket.leave & socket.join
+    users.push({ username: socket.name, id: socket.id });
+    console.log(users);
 
-httpServer.listen(port, () =>{
-    console.log(`server is running on: ${port}`);
-})
+    io.emit("update users", users, users.length);
+    socket.broadcast.emit("user joined", socket.name);
+  });
+  socket.on("sendMessage", (message) => {
+    let msgObject = {
+      message: msg,
+      user: socket.name,
+      private: false,
+    };
+    if (room === "") {
+      socket.broadcast.emit("chat message", msgObject);
+    } else {
+      msgObject.private = true;
+      socket.to(room).emit("chat message", msgObject);
+    }
+  });
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user disconnected", socket.name);
+    users.splice(users.indexOf(socket.name), 1); //Remove from users array
+    io.emit("update users", users, users.length);
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log(`Server is running on: ${port}`);
+});
